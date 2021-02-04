c8316eba3cd6420e9b29aec7e6e00b9f0fcc7d87
yaroslav.benin <yaroslav.benin@sk.com>
2021-02-04 17:57:48 +0300

    And-or mode.

--- decorator_descriptor.py
+++ decorator_descriptor.py
@@ -12,24 +12,33 @@ class Equal(Comparator):
 
 
 class ConditionalRunner:
-    def __init__(self, obj, conditions_list, additional_func, working_method):
+    def __init__(self, logic_mode, obj, conditions_list, additional_func, working_method):
+        self.logic_mode = logic_mode
         self.obj = obj
         self.conditions_list = conditions_list
         self.additional_func = additional_func
         self.working_method = working_method
 
     def __call__(self, *args, **kwargs):
-        for get_state_value, compare_to in self.conditions_list:
-            comparator = get_state_value()
-            if not comparator.compare(self.obj, compare_to):
-                break
-        else:
-            self.additional_func(self.obj)
+        if self.logic_mode == "and":
+            for get_state_value, compare_to in self.conditions_list:
+                comparator = get_state_value()
+                if not comparator.compare(self.obj, compare_to):
+                    break
+            else:
+                self.additional_func(self.obj)
+        elif self.logic_mode == "or":
+            for get_state_value, compare_to in self.conditions_list:
+                comparator = get_state_value()
+                if comparator.compare(self.obj, compare_to):
+                    self.additional_func(self.obj)
+                    break
         return self.working_method(self.obj, *args, **kwargs)
 
 
 class ConditionalRunWrapper:
-    def __init__(self, conditions_list, additional_func):
+    def __init__(self, conditions_list, additional_func, logic_mode):
+        self.logic_mode = logic_mode
         self.conditions_list = conditions_list
         self.additional_func = additional_func
         self._obj = None
@@ -44,7 +53,7 @@ class ConditionalRunWrapper:
         self._obj = value
 
     def __call__(self, working_method):
-        return ConditionalRunner(self.obj, self.conditions_list, self.additional_func, working_method)
+        return ConditionalRunner(self.logic_mode, self.obj, self.conditions_list, self.additional_func, working_method)
 
 
 class StateValueInternal(object):
@@ -76,10 +85,12 @@ class ModelDecorator(object):
         return obj
 
     @staticmethod
-    def conditional_run(comparison_list, additional_func):
-        """:param comparison_list: [(get_state_value, compare_to), ...]
+    def conditional_run(comparison_list, additional_func, logic_mode='and'):
         """
-        return ConditionalRunWrapper(comparison_list, additional_func)
+        :param logic_mode: "and" or "or", how to compare comparison_list.
+        :param comparison_list: [(get_state_value, compare_to), ...]
+        """
+        return ConditionalRunWrapper(comparison_list, additional_func, logic_mode)
 
     @staticmethod
     def state_value(name):
@@ -89,12 +100,12 @@ class ModelDecorator(object):
 class WorkingClass(object):
     @ModelDecorator.state_value("My state value")
     def my_state_value(self):
-        return 16
+        return 18
 
     def additional_func(self):
         print("I'm additional")
 
-    @ModelDecorator.conditional_run([(my_state_value.equal, 16), (my_state_value.equal, 16)], additional_func)
+    @ModelDecorator.conditional_run([(my_state_value.equal, 16), (my_state_value.equal, 18)], additional_func, "or")
     def working_method(self, msg):
         print(f"I'm working with {msg}! ")
         return f"I worked {msg}"
